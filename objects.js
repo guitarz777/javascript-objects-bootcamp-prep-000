da234627f52800b831d270bd21c46e3f58af1fce
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-03 23:32:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,18 @@
 var playlist = {Seether : "Broken"}
 
 function updatePlaylist (playlist, artist, song){
+<<<<<<< HEAD
   playlist[artist] = song;
   return playlist;
 }
 
 function removeFromPlaylist (playlist, artist){
   return delete playlist[artist];
+=======
+  playlist[artist] = song
+}
+
+function removeFromPlaylist (playlist, artist){
+  delete playlist.artist;
+>>>>>>> 3f74534d3206d2017e64aa9cc2b11be6a31415d9
 }
